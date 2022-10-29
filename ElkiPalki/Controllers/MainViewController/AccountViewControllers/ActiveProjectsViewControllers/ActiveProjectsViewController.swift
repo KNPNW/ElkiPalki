5d31f49f50d9bf6195b900025c743d352284fa87
Kirill P <kpadalitsa@gmail.com>
2022-10-29 14:57:42 +0300

    v 0.5.0 fix activeProjectsViewController

--- ElkiPalki/Controllers/MainViewController/AccountViewControllers/ActiveProjectsViewControllers/ActiveProjectsViewController.swift
+++ ElkiPalki/Controllers/MainViewController/AccountViewControllers/ActiveProjectsViewControllers/ActiveProjectsViewController.swift
@@ -1,12 +1,27 @@
 import UIKit
 
 class ActiveProjectsViewController: UIViewController {
+    
+    private lazy var collectionView: ActiveProjectCollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collectionView = ActiveProjectCollectionView(frame: .zero, collectionViewLayout: layout)
+        return collectionView
+    }()
+    
+    var models = [ActiveProjectCollectionView.ActiveProjectOptions]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = NSLocalizedString("Active projects", comment: "")
-        // Do any additional setup after loading the view.
+        
+        configure()
+        
+        view.addSubview(collectionView)
+        
+        setConstraints()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -20,19 +35,20 @@ class ActiveProjectsViewController: UIViewController {
     }
     
     private func setConstraints () {
-        
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
-
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configure() {
+        models.append(ActiveProjectCollectionView.ActiveProjectOptions(main: ActiveProjectCollectionView.mainActiveProjectOptions(numberContract: "231231", dateOfConclusion: "01.02.2022"), additional: ActiveProjectCollectionView.additionalActiveProjectOptions(responsibleManager: "Золотиков Максим Юрьевич", dateStartOfConstruction: "Не указано", customer: "Коваленко Владимир Игорьевич", contractStage: "Ожидает оплаты")))
+        
+        models.append(ActiveProjectCollectionView.ActiveProjectOptions(main: ActiveProjectCollectionView.mainActiveProjectOptions(numberContract: "322133", dateOfConclusion: "10.11.2021"), additional: ActiveProjectCollectionView.additionalActiveProjectOptions(responsibleManager: "Иванов Артем Евгеньевич", dateStartOfConstruction: "28.10.2022", customer: "Коваленко Владимир Игорьевич", contractStage: "Строится")))
+        
+        models.append(ActiveProjectCollectionView.ActiveProjectOptions(main: ActiveProjectCollectionView.mainActiveProjectOptions(numberContract: "231231", dateOfConclusion: "10.11.2022"), additional: ActiveProjectCollectionView.additionalActiveProjectOptions(responsibleManager: "Иванов Артем Евгеньевич", dateStartOfConstruction: "18.11.2022", customer: "Коваленко Владимир Игорьевич", contractStage: "Ожидает строительства")))
+        
+        collectionView.models = models
     }
-    */
 
 }
